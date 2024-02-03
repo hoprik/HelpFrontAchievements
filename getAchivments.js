a45891480414095d23a81180a08a0a36520a96c8
hoprik <42morozov42@gmail.com>
2024-02-04 00:32:48 +0300

    zvz

---
+++ getAchivments.js
@@ -0,0 +1,229 @@
+let source = [{
+    title: "Стартуем",
+    desc: "қҴ\xa4ӄҹҷҼӅӆӄҴӊҼӒ\xa4ӁҴ\xa4ӅҴҽӆҹ",
+    src: "GLHF",
+    reward: 50,
+    exp: 10
+}, {
+    title: "Ночная пчёлка",
+    desc: "қҴ\xa4Ӆҵӂӄ\xa4ҹҺҹҸҹӁҹҶӁӂҽ\xa4ӁҴҷӄҴҸӏ\xa4Ӆ\xa4\xb4\xb4\xbe\xb4\xb4\xa4Ӄӂ\xa4\xb4\xb4\xbe\xb4\xb9",
+    src: "NightButterfly",
+    reward: 100,
+    exp: 50
+}, {
+    title: "Хорошая попытка",
+    desc: "қҴ\xa4ӃӇӅӆӂҽ\xa4ӄҹһӇҿӐӆҴӆ\xa4Ҷ\xa4ӅӃӄҴҶӂӋӁҼҾҹ",
+    src: "GG",
+    reward: 20,
+    exp: 10
+}, {
+    title: "HackerMan",
+    desc: "қҴ\xa4ӁҴӉӂҺҸҹӁҼҹ\xa4ӅҾӄӏӆӂҷӂ\xa4ӑҿҹӀҹӁӆҴ\xa4ӁҴ\xa4ӅҴҽӆҹ\xa4Ҽ\xa4ҾҿҼҾ\xa4Ӄӂ\xa4ӁҹӀӇ",
+    src: "HackerMan",
+    reward: 500,
+    exp: 500
+}, {
+    title: "Постоянный клиент",
+    desc: "қҴ\xa4ӃӂӅҹӍҹӁҼҹ\xa4ӅҴҽӆҴ\xa4Ӆ\xa4ӃӂӁҹҸҹҿӐӁҼҾҴ\xa4Ӄӂ\xa4ҶӂӅҾӄҹӅҹӁӐҹ",
+    src: "DaysSeven",
+    reward: 500,
+    exp: 200
+}, {
+    title: "Шопоголик",
+    desc: "қҴ\xa4ӃҹӄҶӇӒ\xa4ӃӂҾӇӃҾӇ\xa4Ҷ\xa4ӀҴҷҴһҼӁҹ",
+    src: "Shoping",
+    reward: 50,
+    exp: 50
+}, {
+    title: "Скрудж Макдак",
+    desc: "қҴ\xa4ӁҴҾӂӃҿҹӁҼҹ\xa4\xb5\xb4\xb4\xb4\xb4\xa4ӅҹӄҹҵӄҴ",
+    src: "Money",
+    reward: 50,
+    exp: 500
+}, {
+    title: "На стиле",
+    desc: "қҴ\xa4ӃӂҾӇӃҾӇ\xa4ӅӀҴҽҿҼҾҴ\xb0\xa4ӅӆҴӆӇӅҴ\xb0\xa4ӄҴӀҾҼ\xa4Ҽ\xa4ӇӅӆҴӁӂҶҾҼ\xa4ҴҶҴӆҴӄҴ",
+    src: "Style",
+    reward: 100,
+    exp: 200
+}, {
+    title: "Непостоянный",
+    desc: "қҴ\xa4ӅӀҹӁӇ\xa4ӁҼҾӁҹҽӀҴ",
+    src: "ChangeLogin",
+    reward: 200,
+    exp: 100
+}, {
+    title: "Снимаю шляпу",
+    desc: "қҴ\xa4ӁҴӉӂҺҸҹӁҼҹ\xa4ӇӓһҶҼӀӂӅӆҼ\xa4ӁҴ\xa4ӅҴҽӆҹ",
+    src: "SafetySite",
+    reward: 1e3,
+    exp: 1e3
+}, {
+    title: "Волонтёр",
+    desc: "қҴ\xa4ӂӆӃӄҴҶҾӇ\xa4ӂҵӄҴӆӁӂҽ\xa4ӅҶӓһҼ",
+    src: "Volunteer",
+    reward: 50,
+    exp: 50
+}, {
+    title: "Четвертовать",
+    desc: "қҴ\xa4ӃӄӂҶҹҸҹӁҼҹ\xa4ӁҴ\xa4ҷҿҴҶӁӂҽ\xa4ӅӆӄҴӁҼӊҹ\xa4ҵӂҿҹҹ\xa4\xba\xa4ӋҴӅӂҶ",
+    src: "Quarter",
+    reward: 300,
+    exp: 200
+}, {
+    title: "Любопытный",
+    desc: "қҴ\xa4ӂӆҾӄӏӆҼҹ\xa4ӅӆӄҴӁҼӊӏ\xa4\xb8\xb4\xb8",
+    src: "Curious",
+    reward: 100,
+    exp: 200
+}, {
+    title: "Мега мозг",
+    desc: "қҴ\xa4ҾҿҼҾ\xa4Ӄӂ\xa4ӑӆӂҽ\xa4ҴӋҼҶҾҹ",
+    src: "LeftBrain",
+    reward: 30,
+    exp: 50
+}, {
+    title: "Коллекционер",
+    desc: "ҥӂҵӄҴӆӐ\xa4Ҽ\xa4ӃӂҿӇӋҼӆӐ\xa4ӁҴҷӄҴҸӇ\xa4һҴ\xa4ӂҸӁӇ\xa4ҾӂҿҿҹҾӊҼӒ",
+    src: "Collection",
+    reward: 400,
+    exp: 50
+}, {
+    title: "Бывалый",
+    desc: "ңӂҿӇӋҼӆӐ\xa4\xb9\xa4ӇӄӂҶҹӁӐ",
+    src: "ExpPeople",
+    reward: 300,
+    exp: 50
+}, {
+    title: "Богатей",
+    desc: "қҴҸӂӁҴӆҼӆӐ\xa4Ҷ\xa4ӃҹӄҶӏҽ\xa4ӄҴһ",
+    src: "DonateMan",
+    reward: 500,
+    exp: 500
+}, {
+    title: "Шпаргалка",
+    desc: "ҥҾҴӋҴӆӐ\xa4ӄҹӌҹӁҼҹ\xa4ҿӒҵӂҽ\xa4һҴҸҴӋҼ",
+    src: "Solution",
+    reward: 50,
+    exp: 50
+}, {
+    title: "Транжира",
+    desc: "ңӂӆӄҴӆҼӆӐ\xa4\xb5\xb4\xb4\xb4\xb4\xa4ӅҹӄҹҵӄҴ",
+    src: "SoldMoney",
+    reward: 200,
+    exp: 500
+}, {
+    title: "Знай своего врага",
+    desc: "ңӂӅӀӂӆӄҹӆӐ\xa4ӃӄӂӈҼҿӐ\xa4ҸӄӇҷӂҷӂ\xa4ӃӂҿӐһӂҶҴӆҹҿӓ",
+    src: "Enemy",
+    reward: 50,
+    exp: 50
+}, {
+    title: "Предприниматель",
+    desc: "ҖӏӅӆҴҶҼӆӐ\xa4ӑҿҹӀҹӁӆ\xa4ҾӂҿҿҹҾӊҼҼ\xa4ӁҴ\xa4ӄӏӁҾҹ",
+    src: "Salesman",
+    reward: 20,
+    exp: 20
+}, {
+    title: "Собиратель",
+    desc: "ңӂҿӇӋҼӆӐ\xa4ӀҼӁҼӀӇӀ\xa4\xb5\xb4\xa4ҴӋҼҶӂҾ",
+    src: "AchsMan",
+    reward: 200,
+    exp: 100
+}, {
+    title: "Паспортист",
+    desc: "ңӂҿӁӂӅӆӐӒ\xa4һҴӃӂҿӁҼӆӐ\xa4ҵҿӂҾ\xa4\xa6Ң\xa4Ӆҹҵҹ\xa6",
+    src: "AboutMe",
+    reward: 50,
+    exp: 50
+}, {
+    title: "Перегруз",
+    desc: "ңӂҿӁӂӅӆӐӒ\xa4ӅӂҵӄҴӆӐ\xa4ӂҸҼӁ\xa4Ҽһ\xa4ӃӇӁҾӆӂҶ\xa4Ҷ\xa4ҼӁҶҹӁӆҴӄҹ",
+    src: "InventoryFull",
+    reward: 200,
+    exp: 300
+}, {
+    title: "Первый круг",
+    desc: "ҥӂҵӄҴӆӐ\xa4ҾҴҺҸӇӒ\xa4ҾӂҿҿҹҾӊҼӒ\xa4Ӄӂ\xa4ӂҸӁӂӀӇ\xa4ӄҴһӇ",
+    src: "FirstCircle",
+    reward: 1e3,
+    exp: 1e3
+}, {
+    title: "Трейдер",
+    desc: "ҥӂҵӄҴӆӐ\xa4ҵӂҿҹҹ\xa4\xb5\xb4\xb4\xa4һҴӃҼӅҹҽ\xa4Ҷ\xa4ҼӅӆӂӄҼҼ\xa4ӅҹӄҹҵӄҴ\xa4ӁҴ\xa4ҴҾҾҴӇӁӆҹ",
+    src: "Trader",
+    reward: 200,
+    exp: 250
+}, {
+    title: "Не первый день",
+    desc: "ңӂҿӇӋҼӆӐ\xa4\xb5\xb4\xa4ӇӄӂҶҹӁӐ",
+    src: "ExpPeople2",
+    reward: 500,
+    exp: 300
+}, {
+    title: "Олды на месте",
+    desc: "ҕӏӆӐ\xa4һҴӄҹҷҼӅӆӄҼӄӂҶҴӁӁӏӀ\xa4Ӂҹ\xa4ӀҹӁӐӌҹ\xa4\xb7\xb4\xa4ҸӁҹҽ",
+    src: "OldMan",
+    reward: 200,
+    exp: 100
+}, {
+    title: "Двойной тариф",
+    desc: "ҥӂҵӄҴӆӐ\xa4ҿӒҵӇӒ\xa4ҾӂҿҿҹҾӊҼӒ\xa4Ҷӆӂӄӂҽ\xa4ӄҴһ",
+    src: "RetryCollection",
+    reward: 500,
+    exp: 200
+}, {
+    title: "Леньтяй",
+    desc: "ҥҾӂӃҼӄӂҶҴӆӐ\xa4ӃӄҼӀҹӄ\xa4Ҽһ\xa4ӅӃӄҴҶӂӋӁҼҾҴ",
+    src: "CopyPaste",
+    reward: 30,
+    exp: 30
+}, {
+    title: "window.get...",
+    desc: "ҖӏһҶҴӆӐ\xa4Ӌҹӄҹһ\xa4ҾӂӁӅӂҿӐ\xa4ӈӇӁҾӊҼӒ\xa4Ҷ\xa4ӂҵӎҹҾӆҹ\xa4\xfb\xed\xf2\xe8\xf3\xfb\xb0\xa4Ҹҿӓ\xa4ӃӂҿӇӋҹӁҼӓ\xa4ӑӆӂҽ\xa4ҴӋҼҶҾҼ",
+    src: "DevTools",
+    reward: 50,
+    exp: 50
+}];
+
+document.querySelector("button").addEventListener("click", e=>{
+    const input = document.querySelector("input").value
+    const mySelect = document.querySelector("select").value
+    if (mySelect == "all"){
+        source.forEach(elem=>{
+            gen(input, elem.src)
+        })
+    }
+    else{
+        gen(input, mySelect)
+    }
+})
+
+
+function gen(cookie, elem) {
+    const options = {
+        method: 'POST',
+        headers: {
+          cookie: '_ym_uid=1705508036168852481; _ym_d=1706113600; _ym_isad=2; _ym_visorc=w',
+          authority: 'www.helpfront.ru',
+          accept: '*/*',
+          'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+          'content-type': 'application/json',
+          origin: 'https://www.helpfront.ru',
+          referer: 'https://www.helpfront.ru/account/profile',
+          'sec-ch-ua': '^\^Not',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '^\^Windows^^',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+        },
+        body: `{"ach":"${elem}","_id":"${cookie}","isLocal":false}`
+      };
+      
+      fetch('https://www.helpfront.ru/api/getAchievement', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
